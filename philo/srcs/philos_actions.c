3f25786ab69180e5628348b487411272ac903ccd
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2023-04-26 22:38:51 +0200

    philos : delay laying forks
    while it is usually not an issue to lay forks a bit early, this could cause two philos
    to seemingly duplicate forks during slowdowns (very notable under valgrind)
    No forks were duplicated, it was merely the statu messages being delayed.
    But now this is no longer an issue.

--- philo/srcs/philos_actions.c
+++ philo/srcs/philos_actions.c
@@ -6,7 +6,7 @@
 /*   By: OrioPrisco <47635210+OrioPrisco@users      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 17:32:44 by OrioPrisc         #+#    #+#             */
-/*   Updated: 2023/04/25 20:17:26 by OrioPrisc        ###   ########.fr       */
+/*   Updated: 2023/04/26 22:38:18 by orio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,10 +38,14 @@ _Bool	philo_eat(t_philo_data *philo)
 
 _Bool	philo_sleep(t_philo_data *philo)
 {
+	if (philo_say(philo, SLEEP))
+	{
+		lay_fork(&philo->left_fork);
+		lay_fork(&philo->right_fork);
+		return (1);
+	}
 	lay_fork(&philo->left_fork);
 	lay_fork(&philo->right_fork);
-	if (philo_say(philo, SLEEP))
-		return (1);
 	wait_ms(philo->params->time_to_slp);
 	return (0);
 }
